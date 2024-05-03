9fd533a52b13cc593d807cf9099859ec8ab77ad5
Elson Santos <elson.de.souza.santos1@ibm.com>
2024-05-03 15:20:59 -0300

    usando headers

--- src/main/java/org/acme/camel/IntegracaoArquivo.java
+++ src/main/java/org/acme/camel/IntegracaoArquivo.java
@@ -1,6 +1,7 @@
 package org.acme.camel;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.file.FileConstants;
 
 public class IntegracaoArquivo extends RouteBuilder {
     @Override
@@ -8,6 +9,7 @@ public class IntegracaoArquivo extends RouteBuilder {
         from("file:{{diretorioEntrada}}?delay=5000")
                 .routeId("integracao-arquivo")
                 .log("Processando o arquivo: ${file:name}")
-                .to("file:{{diretorioSaida}}");
+                //.setHeader(FileConstants.FILE_NAME, simple("${date:now:ddMMyy_HHmmss}_${file:name}"))
+                .to("file:{{diretorioSaida}}?fileName=${date:now:ddMMyy_HHmmss}_${file:name}");
     }
 }
